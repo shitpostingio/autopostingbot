347fd5635ac1524eca197d0e3fe8b6679e9ea28b
Gianguido Sora <g.sora4@gmail.com>
2018-03-11 18:45:44 +0100

    piccolo file con funzioni di pretty-printing per errori

---
+++ utility/pretty_print_errors.go
@@ -0,0 +1,17 @@
+package utility
+
+import (
+	"log"
+
+	"github.com/fatih/color"
+)
+
+// PrettyError prints an error with a pleasant red text color
+func PrettyError(err error) {
+	color.Red("ERROR - %s", err.Error())
+}
+
+// PrettyFatal prints an error with a pleasant red text color, then exits
+func PrettyFatal(err error) {
+	log.Fatal(color.RedString("ERROR - %s", err.Error()))
+}
