27bc8f4dae305662b107ba44b663b1834fe93330
Gianguido Sora <g.sora4@gmail.com>
2018-03-11 18:44:44 +0100

    aggiunta struttura base manca ancora la gestione dei comandi, che deve ancora essere discussa in #4. Fix #2

--- main.go
+++ main.go
@@ -1,18 +1,74 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
+	"net/http"
 
+	"github.com/fatih/color"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"gitlab.com/shitposting/autoposting-bot/utility"
+)
+
+var (
+	// parsed config file
+	config Config
+
+	// config file path, if not specified it will read
+	// ./config.toml
+	configFilePath string
+	Version        string
+	Build          string
+	err            error
+	debug          bool
 )
 
 func main() {
-	_, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	setupCLIParams()
+	color.Green("Shitposting autoposting-bot version %s, build %s", Version, Build)
+	color.Yellow("INFO - reading configuration file located at %s", configFilePath)
+	config, err = ReadConfigFile(configFilePath)
 	if err != nil {
-		log.Panic(err)
+		utility.PrettyFatal(err)
 	}
 
-	fmt.Println(ReadConfigFile("./config_example.toml"))
+	// setup a Telegram bot API instance
+	bot, err := tgbotapi.NewBotAPI(config.BotToken)
+	if err != nil {
+		utility.PrettyFatal(err)
+	}
 
+	// should we activate debug output?
+	bot.Debug = debug
+
+	color.Yellow("Authorized on account %s", bot.Self.UserName)
+
+	// set webhook to an adequate value
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(config.WebHookURL()))
+	if err != nil {
+		utility.PrettyFatal(err)
+	}
+
+	updates := bot.ListenForWebhook(config.WebHookPath())
+
+	go startServer()
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
+	}
+
+}
+
+func setupCLIParams() {
+	flag.StringVar(&configFilePath, "config", "./config.toml", "configuration file path")
+	flag.BoolVar(&debug, "debug", false, "activate all the debug features")
+	flag.Parse()
+}
+
+func startServer() {
+	if config.TLS {
+		go utility.PrettyFatal(http.ListenAndServeTLS(config.BindString(), config.TLSCertPath, config.TLSKeyPath, nil))
+	}
+
+	go utility.PrettyFatal(http.ListenAndServe(config.BindString(), nil))
 }
