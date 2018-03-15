3a4cc6ba04f02018eea52dc2c84b7f594f6d92e4
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2018-03-15 11:54:48 +0100

    Improve users and posts tables 🤙
    - Add type, media and caption to posts according to https://core.telegram.org/bots/api#inputmedia
    
    - Fix telegram_id structure because it was wrong

--- database/migrations/create_posts_table.sql
+++ database/migrations/create_posts_table.sql
@@ -1,8 +1,11 @@
 CREATE TABLE `posts` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL,
+  `type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media` longtext COLLATE utf8mb4_unicode_ci,
+  `caption` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL,
   PRIMARY KEY (`id`),
   KEY `posts_user_id_index` (`user_id`),
   CONSTRAINT `posts_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
