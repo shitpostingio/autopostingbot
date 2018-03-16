8931d2123cff55ed8170a0c8fbc15bc9171a2b00
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2018-03-16 16:34:31 +0100

    Add M:M relationship between category and post
    An update mentioned in  #14

--- database/entities/category.go
+++ database/entities/category.go
@@ -8,6 +8,7 @@ import (
 type Category struct {
 	ID        uint      `gorm:"AUTO_INCREMENT"`
 	Name      string    `gorm:"type:varchar(191);not null;unique"` // Unique name for the category
+	Posts     []Post    `gorm:"many2many:category_posts;"`         // Category has and belongs to many posts, use `category_posts` as join table
 	CreatedAt time.Time // Timestamp of the creation inside the database
 	UpdatedAt time.Time // Everytime it's update orm will touch this column
 }
--- database/entities/post.go
+++ database/entities/post.go
@@ -6,10 +6,11 @@ import (
 
 // Post Entity
 type Post struct {
-	ID        uint
-	UserID    uint      `gorm:"index"`             // Foreign key (belongs to), tag `index` will create index for this column
-	Type      string    `gorm:"type:varchar(191)"` // Type from telegram API
-	Media     string    `gorm:"type:longtext"`     // The URL of the media from telegram API
-	Caption   string    `gorm:"type:varchar(192)"` // The caption of the current post, aka the test under the media
-	CreatedAt time.Time // Timestamp of the creation inside the database
+	ID         uint
+	UserID     uint       `gorm:"index"`                     // Foreign key (belongs to), tag `index` will create index for this column
+	Type       string     `gorm:"type:varchar(191)"`         // Type from telegram API
+	Media      string     `gorm:"type:longtext"`             // The URL of the media from telegram API
+	Caption    string     `gorm:"type:varchar(192)"`         // The caption of the current post, aka the test under the media
+	Categories []Category `gorm:"many2many:category_posts;"` // Post has and belongs to many categories, use `category_posts` as join table
+	CreatedAt  time.Time  // Timestamp of the creation inside the database
 }
