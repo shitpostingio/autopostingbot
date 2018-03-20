699c8fcc89be9a80882bd65aa2a0101adb07028e
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2018-03-20 16:50:41 +0100

    Add tests ✌️

---
+++ database/entities/post_test.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"reflect"
+	"testing"
+
+	_ "github.com/jinzhu/gorm/dialects/sqlite" // We need SQLite to perform migrations
+)
+
+func TestPostMayBeRelatedToCategories(t *testing.T) {
+	t.Log("Check if a single Post has a collection called Categories with Post")
+	post := new(Post)
+	kind := reflect.TypeOf(post.Categories).Kind()
+
+	if kind != reflect.Slice {
+		t.Errorf("Expected slice, found %s instead.", kind)
+	}
+}
---
+++ database/entities/user_test.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"reflect"
+	"testing"
+
+	_ "github.com/jinzhu/gorm/dialects/sqlite" // We need SQLite to perform migrations
+)
+
+func TestUserMayBeRelatedToPosts(t *testing.T) {
+	t.Log("Check if user can access to every posts he has made")
+	user := new(User)
+	kind := reflect.TypeOf(user.Posts).Kind()
+
+	if kind != reflect.Slice {
+		t.Errorf("Expected slice, found %s instead.", kind)
+	}
+}
