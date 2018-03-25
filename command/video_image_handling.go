627642987e823f335ff561410e2a4851f269c480
Gianguido Sora <g.sora4@gmail.com>
2018-03-25 18:42:14 +0200

    do something with each new message

--- command/video_image_handling.go
+++ command/video_image_handling.go
@@ -1,5 +1,10 @@
 package command
 
+import (
+	"gitlab.com/shitposting/autoposting-bot/algo"
+	"gitlab.com/shitposting/autoposting-bot/database/entities"
+)
+
 // MediaType is the type of media we're dealing with
 type MediaType int
 
@@ -9,6 +14,7 @@ const (
 	Video
 )
 
-func saveMedia(fileID string, caption string, mediaType MediaType) {
-	// TODO: handle video post creation
+// saveMedia sends the media identified by the fileID to the Manager
+func saveMedia(fileID string, caption string, mediaType MediaType, manager *algo.Manager) {
+	manager.AddChannel <- entities.Post{Media: fileID, Caption: caption}
 }
