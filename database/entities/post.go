dfbe3e16cf4cb24536fe1aabb4c8d51b48b55464
Gianguido Sora` <g.sora4@gmail.com>
2018-03-29 15:38:17 +0200

    explicitly use utf8

--- database/entities/post.go
+++ database/entities/post.go
@@ -6,11 +6,12 @@ import (
 
 // Post Entity
 type Post struct {
-	ID         uint       `gorm:"primary_key"`
-	UserID     uint       `gorm:"index"`                     // Foreign key (belongs to), tag `index` will create index for this column
-	Type       string     `gorm:"type:varchar(191)"`         // Type from telegram API
-	Media      string     `gorm:"type:longtext"`             // The URL of the media from telegram API
-	Caption    string     `gorm:"type:varchar(192)"`         // The caption of the current post, aka the test under the media
+	ID     uint   `gorm:"primary_key"`
+	UserID uint   `gorm:"index"`             // Foreign key (belongs to), tag `index` will create index for this column
+	Type   string `gorm:"type:varchar(191)"` // Type from telegram API
+	Media  string `gorm:"type:longtext"`     // The URL of the media from telegram API
+	//Caption    string     `gorm:"type:varchar(192)"`         // The caption of the current post, aka the test under the media
+	Caption    string     `sql:"type:varchar(192) CHARACTER SET utf8 COLLATE utf8_unicode_ci"`
 	Categories []Category `gorm:"many2many:category_posts;"` // Post has and belongs to many categories, use `category_posts` as join table
 	CreatedAt  time.Time  // Timestamp of the creation inside the database
 }
