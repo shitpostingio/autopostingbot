c435effd7f058ddaded99edcc6920936af8e17e5
Antonio Busillo <busillo.antonio@gmail.com>
2018-08-06 15:13:52 +0200

    Fixed a problem with a return in if statement

--- command/command.go
+++ command/command.go
@@ -110,7 +110,6 @@ func addOcCredit(msg *tgbotapi.Message, api *tgbotapi.BotAPI, manager *algo.Mana
 
 	if len(words) < 1 {
 		utility.SendTelegramReply(int(msg.Chat.ID), msg.MessageID, api, "No arguments in /credit!\n\n\xF0\x9F\x93\x96 Usage: /credit <name> <LINK(with http)> \\[OPTIONAL-CAPTION]\n\n\xE2\x9E\xA1 Use /thanks if you want to give credit for a forwarded content!")
-		return
 	} else {
 		var captionstatus bool
 
