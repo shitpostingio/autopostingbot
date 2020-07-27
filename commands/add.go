552bd7b640e73b3a0f79546e0c47a1d08307b72d
Alessandro Pomponio <pomponio.alessandro@hotmail.it>
2020-07-27 17:34:40 +0200

    Improvements in command handlers

--- commands/add.go
+++ commands/add.go
@@ -1,32 +1,44 @@
 package commands
 
 import (
+	"errors"
 	"github.com/zelenin/go-tdlib/client"
 	"gitlab.com/shitposting/autoposting-bot/analysisadapter"
 	"gitlab.com/shitposting/autoposting-bot/api"
+	"gitlab.com/shitposting/autoposting-bot/caption"
 	"gitlab.com/shitposting/autoposting-bot/documentstore/dbwrapper"
 	"gitlab.com/shitposting/autoposting-bot/documentstore/entities"
 )
 
-type AddCommandHandler struct {
-}
+type AddCommandHandler struct {}
 
 func (AddCommandHandler) Handle(arguments string, message, replyToMessage *client.Message) error {
 
+	//
+	if replyToMessage == nil {
+		_, _ = api.SendPlainReplyText(message.ChatId, message.Id, "This command needs to be used in reply to a media file")
+		return errors.New("reply to message nil")
+	}
+
+	//
 	fileInfo, err := api.GetMediaFileInfo(replyToMessage)
 	if err != nil {
+		_, _ = api.SendPlainReplyText(message.ChatId, message.Id, "This command needs to be used in reply to a media file")
 		return err
 	}
 
+	//
 	mediaType := api.GetTypeFromMessageType(replyToMessage.Content.MessageContentType())
-
 	fingerprint, err := analysisadapter.Request(fileInfo.Local.Path, mediaType, fileInfo.Remote.UniqueId)
 	if err != nil {
+		_, _ = api.SendPlainReplyText(message.ChatId, message.Id, "Unable to get media fingerprint")
 		return err
 	}
 
+	//
 	avg, sum := entities.GetHistogramAverageAndSum(fingerprint.Histogram)
 
+	//
 	media := entities.Media{
 		Type:             mediaType,
 		TdlibID:          fileInfo.Id,
@@ -38,8 +50,22 @@ func (AddCommandHandler) Handle(arguments string, message, replyToMessage *clien
 		PHash:            fingerprint.PHash,
 	}
 
-	//TODO: VEDERE CHE FARE CON LA CAPTION
-	err = dbwrapper.AddPost(replyToMessage.SenderUserId, media, "")
+	// If the message is a forward, remove the caption
+	postCaption := ""
+
+	// if the message is not a forward, get the caption
+	if message.ForwardInfo == nil {
+		ft := api.GetMediaFormattedText(replyToMessage)
+		postCaption = caption.ToHTMLCaption(ft)
+	}
+
+	err = dbwrapper.AddPost(replyToMessage.SenderUserId, media, postCaption)
+	if err != nil {
+		_, _ = api.SendPlainReplyText(message.ChatId, message.Id, "Error while trying to add the post")
+	} else {
+		_, _ = api.SendPlainReplyText(message.ChatId, message.Id, "Media added correctly!")
+	}
+
 	return err
 
 }
--- commands/caption.go
+++ commands/caption.go
@@ -1,7 +1,8 @@
 package commands
 
 import (
-	"fmt"
+	"errors"
+	log "github.com/sirupsen/logrus"
 	"github.com/zelenin/go-tdlib/client"
 	"gitlab.com/shitposting/autoposting-bot/api"
 	"gitlab.com/shitposting/autoposting-bot/caption"
@@ -13,31 +14,40 @@ type CaptionCommandHandler struct {
 
 func (CaptionCommandHandler) Handle(arguments string, message, replyToMessage *client.Message) error {
 
-	fmt.Println("Arguments: ", arguments)
-
 	//
-	newCaption := ""
-
-	//
-	if arguments != "" {
-
-		text := message.Content.(*client.MessageText).Text
-		msgLengthDifference := len(text.Text) - len(arguments)
-		fmt.Println("Text: ", text.Text, " len diff: ", msgLengthDifference)
-
-		newCaption = caption.ToHTMLCaption(text)
-		fmt.Println("NewCaption: ", newCaption)
-		newCaption = newCaption[msgLengthDifference:]
-		fmt.Println("NC should be: ", newCaption)
-
+	if replyToMessage == nil {
+		_, _ = api.SendPlainReplyText(message.ChatId, message.Id, "This command needs to be used in reply to a media file")
+		return errors.New("reply to message nil")
 	}
 
+	//
 	fi, err := api.GetMediaFileInfo(replyToMessage)
 	if err != nil {
+		_, _ = api.SendPlainReplyText(message.ChatId, message.Id, "This command needs to be used in reply to a media file")
 		return err
 	}
 
-	fmt.Println("FI: ", fi.Remote.UniqueId)
-	return dbwrapper.UpdatePostCaptionByUniqueID(fi.Remote.UniqueId, newCaption)
+	//
+	newCaption := ""
+	if arguments != "" && message.Content.MessageContentType() == client.TypeMessageText {
+
+		//
+		text := message.Content.(*client.MessageText).Text
+		msgLengthDifference := len(text.Text) - len(arguments)
+		log.Debugln("Text: ", text.Text, " len diff: ", msgLengthDifference)
+
+		//
+		newCaption = caption.ToHTMLCaptionWithCustomStart(text, msgLengthDifference)
+		newCaption = newCaption[msgLengthDifference:]
+		log.Debugln("new caption is: ", newCaption)
+
+	}
+
+	// Save new caption to database
+	err = dbwrapper.UpdatePostCaptionByUniqueID(fi.Remote.UniqueId, newCaption)
+
+	// Send how the new post looks like
+	_ = PreviewCommandHandler{}.Handle("", message, replyToMessage)
+	return err
 
 }
