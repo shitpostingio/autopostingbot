3e8e5d7a25b939896d9998ed2561964ad8d07b53
Alessandro Pomponio <pomponio.alessandro@hotmail.it>
2020-09-02 14:04:25 +0200

    Add comments to package repository

--- repository/repository.go
+++ repository/repository.go
@@ -6,7 +6,12 @@ import (
 )
 
 var (
+	// Config contains all the configuration structures.
 	Config *structs.Config
+
+	// Tdlib is the Telegram client instance.
 	Tdlib  *client.Client
+
+	// Me represents the current bot as a Telegram client.User.
 	Me     *client.User
 )
