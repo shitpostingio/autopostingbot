08e074067b41e5377c65aeb0ae183eb797ff7902
Rutori <saintmishin@gmail.com>
2021-04-02 10:52:02 +0300

    Config unmarshal now returns all field errors (#10)
    * Config unmarshal now returns all field errors
    
    * Removed unnecessary variable
    
    * Added test case for missing reloadable value
    
    * Updated variable names, improved error concatenation process, made functions return nil slice instead of an empty one for convenient comparison

--- config/checker.go
+++ config/checker.go
@@ -2,45 +2,67 @@ package config
 
 import (
 	"fmt"
-	"github.com/shitpostingio/autopostingbot/config/structs"
 	"reflect"
+
+	"github.com/shitpostingio/autopostingbot/config/structs"
 )
 
 // checkMandatoryFields uses reflection to see if there are
 // mandatory fields with zero value.
+// When using with isReload flag, only checks for fields with reloadable:"true" tag
 func checkMandatoryFields(isReload bool, config structs.Config) error {
-	return checkStruct(isReload, reflect.TypeOf(config), reflect.ValueOf(config))
+	resultingError := ""
+	for _, err := range checkStruct(isReload, "root", reflect.TypeOf(config), reflect.ValueOf(config)) {
+		resultingError = fmt.Sprintf("%s\n%s", resultingError, err.Error())
+	}
+
+	if resultingError == "" {
+		return nil
+	}
+
+	return fmt.Errorf("%s", resultingError)
 }
 
 // checkStruct explores structures recursively and checks if
 // struct fields have a zero value.
-func checkStruct(isReload bool, typeToCheck reflect.Type, valueToCheck reflect.Value) (err error) {
-
+func checkStruct(isReload bool, parent string, typeToCheck reflect.Type, valueToCheck reflect.Value) (errors []error) {
+	errors = make([]error, 0)
 	for i := 0; i < typeToCheck.NumField(); i++ {
-
 		currentField := typeToCheck.Field(i)
 		currentValue := valueToCheck.Field(i)
-
 		switch currentField.Type.Kind() {
+		case
+			reflect.Uintptr,
+			reflect.UnsafePointer,
+			reflect.Ptr:
+			continue
+
 		case reflect.Struct:
-			err = checkStruct(isReload, currentField.Type, currentValue)
+			if errs := checkStruct(isReload, currentField.Name, currentField.Type, currentValue); errs != nil {
+				errors = append(errors, errs...)
+			}
+
 		case reflect.Slice:
-			err = checkSlice(isReload, currentField, currentValue)
+			if errs := checkSlice(isReload, fmt.Sprintf("%s in %s", currentField.Name, parent), currentField, currentValue); errs != nil {
+				errors = append(errors, errs...)
+			}
+
 		default:
-			err = checkField(isReload, currentField, currentValue)
+			if err := checkField(isReload, parent, currentField, currentValue); err != nil {
+				errors = append(errors, err)
+			}
 		}
+	}
 
-		if err != nil {
-			return
-		}
-
+	if len(errors) != 0 {
+		return errors
 	}
 
 	return nil
 }
 
-// checkStruct explores slices recursively and checks they have a zero value.
-func checkSlice(isReload bool, typeToCheck reflect.StructField, sliceToCheck reflect.Value) error {
+// checkSlice explores slices recursively and checks they have a zero value.
+func checkSlice(isReload bool, parent string, typeToCheck reflect.StructField, sliceToCheck reflect.Value) []error {
 
 	//only check reloadable fields if isReload is true
 	if isReload {
@@ -58,38 +80,35 @@ func checkSlice(isReload bool, typeToCheck reflect.StructField, sliceToCheck ref
 	}
 
 	if sliceToCheck.Len() == 0 {
-		return fmt.Errorf("non optional slice field %s had zero length", typeToCheck.Name)
+		return []error{fmt.Errorf("non optional slice field %s in %s had zero length", typeToCheck.Name, parent)}
 	}
 
-	var err error
+	errors := make([]error, 0)
 	for i := 0; i < sliceToCheck.Len(); i++ {
-
 		item := sliceToCheck.Index(i)
 		if item.Kind() == reflect.Struct {
-			err = checkStruct(isReload, reflect.TypeOf(item), reflect.ValueOf(item))
-		} else {
-
-			zeroValue := reflect.Zero(item.Type())
-			if item.Interface() == zeroValue.Interface() {
-				return fmt.Errorf("non optional field %s had zero value at index %d", typeToCheck.Name, i)
+			if errs := checkStruct(isReload, parent, reflect.TypeOf(item.Interface()), reflect.ValueOf(item.Interface())); errs != nil {
+				errors = append(errors, errs...)
 			}
-
+			continue
 		}
 
-		if err != nil {
-			return err
+		zeroValue := reflect.Zero(item.Type())
+		if item.Interface() == zeroValue.Interface() {
+			errors = append(errors, fmt.Errorf("non optional field %s had zero value at index %d", typeToCheck.Name, i))
 		}
+	}
 
+	if len(errors) != 0 {
+		return errors
 	}
 
 	return nil
-
 }
 
 // checkField checks if a field is optional or a webhook field
 // if it isn't, it checks if the field has a zero value.
-func checkField(isReload bool, typeToCheck reflect.StructField, valueToCheck reflect.Value) error {
-
+func checkField(isReload bool, section string, typeToCheck reflect.StructField, valueToCheck reflect.Value) error {
 	//only check reloadable fields if isReload is true
 	if isReload {
 
@@ -109,7 +128,7 @@ func checkField(isReload bool, typeToCheck reflect.StructField, valueToCheck ref
 	zeroValue := reflect.Zero(typeToCheck.Type)
 
 	if valueToCheck.Interface() == zeroValue.Interface() {
-		return fmt.Errorf("non optional field %s had zero value", typeToCheck.Name)
+		return fmt.Errorf("non optional field %s in section %s had zero value", typeToCheck.Name, section)
 	}
 
 	return nil
